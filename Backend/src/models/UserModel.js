1978e782ab05157fda4629c5679c1251bf75e149
Minh20812 <blossworld12@gmail.com>
2025-03-10 11:13:48 +0700

    initial

---
+++ Backend/src/models/UserModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
---
+++ Backend/src/routes/userRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  createUser,
+  loginUser,
+  loginGoogleUser,
+  logoutCurrentUser,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+  searchUser,
+} from "../controllers/userController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleWare.js";
+
+const router = express.Router();
+
+router.route("/register").post(createUser);
+router.post("/", loginUser);
+router.post("/login-google", loginGoogleUser);
+router.post("/logout", logoutCurrentUser);
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+router.route("/search").get(authenticate, searchUser);
+
+// Route for admin to manage users
+
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
+export default router;
