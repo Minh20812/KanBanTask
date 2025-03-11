e1e05fea6763494194a994081bf3404e5a5c0200
Minh20812 <blossworld12@gmail.com>
2025-03-11 21:03:45 +0700

    update cors

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://kanbantask.vercel.app/"],
+    origin: ["https://kanbantask.vercel.app"],
     // origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
